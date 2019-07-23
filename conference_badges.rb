722ebde6004e10547bb786b4d60c704ffa5e7ad7
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-23 16:02:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator
    batch_badge_creator.each do |attendee|
     "Hello, my name is #{attendee}."
   end
