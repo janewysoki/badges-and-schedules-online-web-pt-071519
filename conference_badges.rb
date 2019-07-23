18b1ae9a27137b2e72f382b35a9647d15ed410d1
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-23 16:16:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,9 @@ end
 def assign_rooms(attendees)
   new_array = []
   attendees.each_with_index do |attendee, index|
-  "Hello, #{name}! You'll be assigned to room #{index}!"
+  new_array << "Hello, #{name}! You'll be assigned to room #{index+1}!"
 end
+new_array
 end
 
 def printer
