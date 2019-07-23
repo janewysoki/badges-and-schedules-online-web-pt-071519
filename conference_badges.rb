1ed7d6b122d063619c2f77a32f39945d8dd7f37c
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-23 15:35:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+ puts "Hello, my name is #{name}."
 end
 
 def batch_badge_creator
